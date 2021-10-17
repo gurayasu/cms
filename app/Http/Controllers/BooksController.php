92a36a40173d26de103746d520383bf28f780120
EC2 Default User <ec2-user@ip-172-31-3-87.ap-northeast-1.compute.internal>
2021-10-17 08:54:31 +0000

    first commit

---
+++ app/Http/Controllers/BooksController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Book; //Bookモデルを使えるようにする
+use Validator; //バリデーションを使えるようにする
+use Auth; //認証モデルを使用する
+
+class BooksController extends Controller
+{
+    //本ダッシュボード表示
+    public function index(){
+        $books = Book::where('user_id',Auth::user()->id)->orderBy('created_at', 'asc')->paginate(3);
+        //ddd($books);
+        return view('books',[
+            'books' => $books
+        ]);  //＊＊ ここを修正 ＊＊
+    }
+    
+    //更新画面
+    public function edit($book_id){
+        $books = Book::where('user_id',Auth::user()->id)->find($book_id);
+        return view('booksedit',['book'=>$books]);
+    }
+    
+    //更新
+    public function update(Request $request){
+            //バリデーション
+    $validator = Validator::make($request->all(), [
+        'id'=>'required',
+        'item_name' => 'required|min:3|max:255',
+        'item_number'=>'required|min:1|max:3',
+        'item_amount'=>'required|max:6',
+        'published'=>'required'
+    ]);
+
+    //バリデーション:エラー 
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+    
+    // データ更新
+    $books = Book::where('user_id',Auth::user()->id)->find($request->id);
+    $books->item_name = $request->item_name;
+    $books->item_number = $request->item_number;
+    $books->item_amount = $request->item_amount;
+    $books->published = $request->published;
+    $books->save(); 
+    return redirect('/');
+
+    }
+    
+    //登録
+    public function store(Request $request){
+            //バリデーション
+     $validator = Validator::make($request->all(), [
+        'item_name' => 'required|min:3|max:255',
+        'item_number'=>'required|min:1|max:3',
+        'item_amount'=>'required|max:6',
+        'published'=>'required'
+    ]);
+
+    //バリデーション:エラー 
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+    
+    $file = $request->file('item_img');//file取得
+    if(!empty($file)){
+        $filename = $file->getClientOriginalName();//ファイル名取得
+        $move = $file->move('./upload',$filename);//ファイルを移動
+    }else{
+        $filename="";
+    }
+    
+    
+    // Eloquentモデル（登録処理）
+    $books = new Book;
+    $books->user_id = Auth::user()->id;
+    $books->item_name = $request->item_name;
+    $books->item_number = $request->item_number;
+    $books->item_amount = $request->item_amount;
+    $books->item_img = $filename;
+    $books->published = $request->published;
+    $books->save(); 
+    return redirect('/')->with('message','本登録が完了しました');
+    }
+    
+    //削除
+    public function destroy(Book $book){
+        $book->delete();
+        return redirect('/');
+    }
+    
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
+
+}
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+use App\Book;
+use Illuminate\Http\Request;
+
+/**
+* 本ダッシュボード表示
+*/
+Route::get('/', 'BooksController@index')->name('login');
+
+/**
+* 本を登録
+*/
+Route::post('/books','BooksController@store');
+
+
+// 更新画面
+Route::post('booksedit/{books}','BooksController@edit');
+
+// 更新処理
+Route::post('/books/update','BooksController@update');
+
+/**
+* 本を削除 
+*/
+Route::delete('/book/{book}','BooksController@destroy');
+
+
+//Auth
+Auth::routes();
+Route::get('/home', 'BooksController@index')->name('home');
